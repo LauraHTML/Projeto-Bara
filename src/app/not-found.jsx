03ab6aea6cc09ebcad6dd2622653f26522ffd3d4
LauraHTML <laura.senai03@gmail.com>
2026-01-05 15:03:36 -0300

    Link funionando navbar

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -4,17 +4,17 @@ import { Button } from '@/components/ui/button';
 
 export default function NotFound() {
   return (<>
-    <div className='min-h-full   bg-secondary p-4 flex-col centralizar gap-8 lg:gap-0 text-background'>
+    <div className='md:min-h-screen min-h-full lg:min-h-full bg-secondary p-4 flex-col centralizar gap-8 lg:gap-0 text-background'>
         <div className='centralizar flex-col gap-5 mt-10'>
             <h2 className='text-6xl'>OOPS...</h2>
-            <p>Essa página não existe, vamos voltar para a página inicial.</p>
+            <p className='text-center'>Essa página não existe, vamos voltar para a página inicial.</p>
             <Button>
             <Link href="/">Voltar para página inicial</Link>
             </Button>
         </div>
-        <div className='h-[30rem] m-2 relative'> 
-            <h1 className='text-[25rem] tracking-tight bg-[url(/textura-papel-titulo.png)] bg-clip-text text-transparent bg-ontain bg-center md:ml-6 md:h-[45rem]'>ERRO</h1>
-            <h3 className='text-[10rem] text absolute z-10 text-accent md:left-100 md:top-70'>404</h3>
+        <div className='h-[20rem] md:h-[30rem] m-2 relative'> 
+            <h1 className='text-[10rem] md:text-[25rem] tracking-tight bg-[url(/textura-papel-titulo.png)] bg-clip-text text-transparent bg-ontain bg-center md:ml-6 md:h-[45rem]'>ERRO</h1>
+            <h3 className='text-[4rem] md:text-[10rem] absolute z-10 text-accent left-35 md:left-80 lg:left-100 top-30 md:top-70'>404</h3>
         </div>
     </div>
   </>
--- src/components/nav-menu.jsx
+++ src/components/nav-menu.jsx
@@ -7,30 +7,48 @@ import {
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 
+const navSections = [
+  {
+    title: "Product",
+    links: [
+      {
+        title: "Conheça",
+        href: "/",
+      },
+      {
+        title: "Aplique na sua escola",
+        href: "/baraNaSuaEscola",
+      },
+      {
+        title: "Fale Conosco",
+        href: "/contato",
+      },
+      {
+        title: "Invista",
+        href: "/invistaNaBara",
+      },
+    ],
+  }
+];
+
 export const NavMenu = (props) => (
-  <NavigationMenu {...props}>
-    <NavigationMenuList
-      className="data-[orientation=vertical]:-ms-2 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start">
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-          <Link className="text-background" href="/">Conheça</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link className="text-background"  href="/baraNaSuaEscola">Aplique na sua escola</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link className="text-background"  href="/contato">Fale conosco</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-          <Link className="text-background"  href="/invistaNaBara">Invista</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
+  <>
+  {navSections.map(({ title, links }) => (
+              <NavigationMenu key={title} {...props}>
+                <NavigationMenuList className="data-[orientation=vertical]:-ms-2 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start">
+                  {links.map(({ title, href }) => (
+                    <NavigationMenuItem key={title} className=" rounded-md m-0">
+                      <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+                        <Link href={href} className="text-background">
+                        {title}
+                      </Link>
+                      </NavigationMenuLink>
+                      
+                    </NavigationMenuItem>
+                  ))}
+                </NavigationMenuList>
+              </NavigationMenu>
+            ))}
+  </>
+
 );
